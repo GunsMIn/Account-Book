43c5c8138fb0c7df65dbd3b18b2fb9e8817403f4
KimGunWoo9595 <medneter@naver.com>
2023-01-09 22:56:34 +0900

    Feat : Custom Exception 생성

---
+++ src/main/java/com/payhere/account/exception/ExceptionManager.java
@@ -0,0 +1,25 @@
+package com.payhere.account.exception;
+
+import com.payhere.account.domain.Response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionManager {
+    /**UserException**/
+    @ExceptionHandler(AccountException.class)
+    public ResponseEntity<?> userAppExceptionHandler(AccountException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode(), e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(Response.error("ERROR", errorResponse));
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/payhere/account/exception/UserException.java
@@ -0,0 +1,27 @@
+package com.payhere.account.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserException extends RuntimeException{
+
+    ErrorCode errorCode;
+    private String message; //String.format("UserName:%s", userJoinRequest.getUserName())
+
+
+
+    public UserException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = null;
+    }
+
+    @Override
+    public String toString() {
+        if(message == null) {
+            return errorCode.getMessage();
+        }
+        return String.format("%s. %s", errorCode.getMessage(), message);
+    }
+}
