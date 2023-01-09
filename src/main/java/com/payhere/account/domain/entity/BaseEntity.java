ea1fb330b715c7df57ef85e320dd39472cd40204
KimGunWoo9595 <medneter@naver.com>
2023-01-09 22:59:51 +0900

    Feat : BaseEntity and AccoutException

---
+++ src/main/java/com/payhere/account/domain/entity/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.payhere.account.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private String registeredAt;
+
+    @LastModifiedDate
+    private String updatedAt;
+
+    @PrePersist
+    public void onPrePersist(){
+        this.registeredAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        this.updatedAt = this.registeredAt;
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        this.updatedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    }
+
+}
